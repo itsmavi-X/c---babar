143d18d88e29c84680376d9e2c93ff731b13dde0
Vishal <vishalmavi823@gmail.com>
2025-10-01 11:55:46 +0530

    Added newfile.cpp

---
+++ Code360 Questions/FindUnique.cpp
@@ -0,0 +1,26 @@
+/*Problem statement
+You have been given an integer array/list(ARR) of size N. Where N is equal to [2M + 1].
+
+Now, in the given array/list, 'M' numbers are present twice and one number is present only once.
+
+You need to find and return that number which is unique in the array/list.
+
+ Note:
+Unique element is always present in the array/list according to the given condition.*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int  arr[100];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int ans=0;
+    for(int i=0;i<n;i++){
+        ans=ans^arr[i];
+    }
+    cout<<ans<<endl;
+
+}
